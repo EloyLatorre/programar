dac173db3db9ac2055d3def7f973171cebd5ffb3
EloyLatorre <elatorrebriones@gmail.com>
2023-03-21 12:57:34 +0100

    He cambiado un bucle
    Siggned-off-by: EloyLatorre <elatorrebriones@gmail.com>

---
+++ prueba/src/prueba/Patata.java
@@ -0,0 +1,12 @@
+package prueba;
+
+public class Patata {
+	public static void main(String[] args) {
+        String palabra = "patata";
+
+        for (int i = 0; i < 7; i++) {
+            System.out.println(palabra);
+            palabra += "a";
+        }
+    }
+}
