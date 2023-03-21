4a938611bf8ef33cfed9a22efa8b5a56dba53fb5
EloyLatorre <73794310+EloyLatorre@users.noreply.github.com>
2023-03-21 13:13:52 +0100

    Update Patata.java

--- prueba/src/prueba/Patata.java
+++ prueba/src/prueba/Patata.java
@@ -4,7 +4,7 @@ public class Patata {
 	public static void main(String[] args) {
         String palabra = "patata";
 
-        for (int i = 0; i < 7; i++) {
+        for (int i = 0; i < 8; i++) {
             System.out.println(palabra);
             palabra += "a";
         }
